b786fa0fd1f119bfc9e6f515d7aa72fd242bc0c6
Arthur Machado <thuur51@gmail.com>
2020-06-30 15:16:17 -0300

    Looping do jogo

--- game.py
+++ game.py
@@ -5,5 +5,19 @@ pygame.init()
 larguraTela = 800
 alturaTela = 600
 gamedisplay = pygame.display.set_mode((larguraTela,alturaTela))
-
 clock = pygame.time.Clock()
+black = (0,0,0)
+white = (255,255,255)
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    pygame.display.update()
+    clock.tick(60)
+
+
